80be93e85736f2e946446f0115ec1e017d5747ee
Ross Edwards <redwards64@hotmail.com>
2021-03-20 19:22:46 +0000

    test push

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+    public Label helloWorld;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World!");
+    }
 }
