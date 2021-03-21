1d0715360b845d3ab7d41843a4e19ecd439b6c43
kieren <c0016836@my.shu.ac.uk>
2021-03-21 21:51:29 +0000

    Login Page

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,35 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
-    public Label helloWorld;
+    @FXML
+    private TextField StudentId;
 
-    public void sayHelloWorld(ActionEvent actionEvent) {
-        helloWorld.setText("Hello World!");
+    @FXML
+    private PasswordField Password;
+
+    @FXML
+    private Button SigininButton;
+
+    @FXML
+    private Label Siginin_Page_Label;
+
+    public void SigininButtonONation(ActionEvent event){
+        if (StudentId.getText().isEmpty() && Password.getText().isEmpty()){
+            Siginin_Page_Label.setText("Please Enter A Password And Username");
+        }
+        else{
+            Siginin_Page_Label.setText("both are entered");
+        }
     }
+
 }
