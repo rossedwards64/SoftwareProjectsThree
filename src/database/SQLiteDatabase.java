a67c53955705d55eb4371c879159852e28ee94b6
Ross Edwards <redwards64@hotmail.com>
2021-03-31 03:35:42 +0100

    added student database and basic login functionality

---
+++ src/database/SQLiteDatabase.java
@@ -0,0 +1,40 @@
+package database;
+
+import java.sql.*;
+
+public class SQLiteDatabase {
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            String fileName = "src/database/student_table.db";
+            String url = "jdbc:sqlite:" + fileName;
+            conn = DriverManager.getConnection(url);
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static boolean verifyLogin(String studentID, String studentPassword) {
+        Connection conn = connect();
+        String SQL = "SELECT * FROM StudentInfo WHERE StudentID = ? AND StudentPassword = ? OR (StudentID IS NULL AND StudentPassword IS NULL)";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, studentID);
+            pstmt.setString(2, studentPassword);
+            ResultSet result = pstmt.executeQuery();
+
+            if(result.next()) {
+                System.out.println("Student ID and password are correct");
+                conn.close();
+                return true;
+            } else {
+                System.out.println("Student ID and password are incorrect");
+                return false;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+}
