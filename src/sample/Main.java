a979a6a5a857aca79498918fd1487b04a4cdaf9d
Ross Edwards <redwards64@hotmail.com>
2021-04-12 12:05:39 +0100

    retrieves user id from database when user marks themselves in

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,11 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
